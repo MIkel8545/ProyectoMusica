ae0316b49a492db13d7b819367795401f495445d
miguel <miguel.a.c.va@gmail.com>
2022-09-21 16:58:52 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/projectmusic/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String prueba;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
